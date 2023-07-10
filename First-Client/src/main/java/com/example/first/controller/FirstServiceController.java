9dd910ec40924d09c0db22504a6aa1846a093fdc
eroul-ri <bhr139313@gmail.com>
2023-07-10 14:32:19 +0900

    gateway 테스트용 request 추가

--- First-Client/src/main/java/com/example/first/controller/FirstServiceController.java
+++ First-Client/src/main/java/com/example/first/controller/FirstServiceController.java
@@ -2,12 +2,19 @@ package com.example.first.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 @RestController
 public class FirstServiceController {
 
 	@GetMapping("/first-service/welcome")
 	public String welcome() {
 		return "Hello World, Welcome First Service";
+	}
+
+	@GetMapping("/first-service/message")
+	public String message(@RequestHeader("first-request") String header) {
+		log.info("first-service/message header :: {}", header);
+		return "Hello World in First Service";
 	}
 }
--- Second-Client/src/main/java/com/example/second/controller/SecondServiceController.java
+++ Second-Client/src/main/java/com/example/second/controller/SecondServiceController.java
@@ -2,12 +2,20 @@ package com.example.second.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 public class SecondServiceController {
 
 	@GetMapping("/second-service/welcome")
 	public String welcome() {
 		return "Hello World, Welcome Second Service";
+	}
+
+	@GetMapping("/second-service/message")
+	public String message(@RequestHeader("second-request") String header) {
+		log.info("second-service/message header :: {}", header);
+		return "Hello World in Second Service";
 	}
 }
