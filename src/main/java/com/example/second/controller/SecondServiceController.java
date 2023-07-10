80a3d146346db1d2010acea6c8288d93bc662180
eroul-ri <bhr139313@gmail.com>
2023-07-10 11:35:38 +0900

    zuul second service

---
+++ src/main/java/com/example/second/controller/SecondServiceController.java
@@ -0,0 +1,13 @@
+package com.example.second.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecondServiceController {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Hello World, Welcome Second Service";
+	}
+}
